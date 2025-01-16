47f0c3914344a6455ec9733aa1e240e7947a0a73
Olegioner <sitdh@mail.ru>
2025-01-16 08:01:38 +0700

    add_utility_16012025_0801

---
+++ Reboot_linux_with_ssh.py
@@ -0,0 +1,18 @@
+# Скрипт для перезагрузки ПК с Linux через SSH
+# На ПК с Linux должны быть установлены модули pip и paramiko
+
+# --Установка модулей на примере AltLinux--
+# apt-get install python3-module-pip
+# python3 -m pip install --upgrade pip --proxy
+# pip3 install paramiko
+
+
+import paramiko
+
+client = paramiko.SSHClient()
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.connect('HOSTNAME or IP-address', username='NAME', password='*******')
+
+client.exec_command('sudo reboot')
